b4c8a2033103531b4b717aeef64bd52a3ef67d54
Momanyi Poet <momanyiturner@gmail.com>
2023-10-05 15:30:07 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#30bcd9',
+        secondary: '#024e82',
+        whity: '#bad7f9',
+        darky: '#0c1d42',
+      },
+      fontFamily: {
+        fugaz: ['Fugaz One']
+      }
+    },
   },
   plugins: [],
 }
